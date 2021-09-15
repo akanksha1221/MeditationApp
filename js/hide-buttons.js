528eb373ceea4cfb85b2915bd22869092597231e
akanksha00 <54508862+akanksha00@users.noreply.github.com>
2021-09-15 17:02:19 +0530

    first

---
+++ js/hide-buttons.js
@@ -0,0 +1,21 @@
+const app = document.querySelector(".app");
+
+//const inactiveTime = 3000;
+let mouseLastMoveTime = new Date();
+
+document.addEventListener("mousemove",()=>{
+    mouseLastMoveTime = new Date();
+    app.classList.remove("inactive");
+    document.body.style.cursor="auto";
+
+})
+function deactivateApp(){
+    let now = new Date();
+    let deltaTime = now - mouseLastMoveTime;
+
+    if(deltaTime>=inactiveTime){
+        app.classList.add("inactive");
+    }
+    requestAnimationFrame(deactivateApp);//loop, check every millisecond
+}
+deactivateApp();
